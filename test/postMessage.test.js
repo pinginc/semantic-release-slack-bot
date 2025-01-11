83e7073b6e8eb31c4ff06ac8308683d036754940
Nat Welch <nat@natwelch.com>
2025-01-11 17:25:13 +0000

    better

--- test/postMessage.test.js
+++ test/postMessage.test.js
@@ -2,7 +2,7 @@ import { ifError, rejects } from 'assert'
 import nock from 'nock'
 import postMessage from '../lib/postMessage.js'
 import SemanticReleaseError from '@semantic-release/error'
-import { describe, it } from 'mocha'
+import { describe, it, beforeEach } from 'mocha'
 
 const slackWebhook = 'https://www.webhook.com'
 const slackPostMessageDomain = 'https://slack.com'
@@ -11,8 +11,7 @@ const slackToken = 'token'
 const slackChannel = 'channel'
 
 async function postWebhook(url) {
-  url = url || slackWebhook
-  await postMessage('message', { log: () => undefined }, { slackWebhook: url })
+  await postMessage('message', {}, { slackWebhook: url })
 }
 
 async function postToken(token, channel) {
@@ -26,11 +25,15 @@ async function postToken(token, channel) {
 }
 
 describe('test postMessage with webhook', () => {
+  beforeEach(() => {
+    nock.cleanAll()
+  })
+
   it('should pass if response is 200 "ok"', async () => {
     nock(slackWebhook)
       .post('/')
       .reply(200, 'ok')
-    ifError(await postWebhook())
+    ifError(await postWebhook(slackWebhook))
   })
 
   it('should fail if response text is not "ok"', async () => {
@@ -39,7 +42,7 @@ describe('test postMessage with webhook', () => {
       .post('/')
       .reply(200, response)
     await rejects(
-      postWebhook(),
+      postWebhook(slackWebhook),
       new SemanticReleaseError(response, 'INVALID SLACK COMMAND')
     )
   })
@@ -50,7 +53,7 @@ describe('test postMessage with webhook', () => {
       .post('/')
       .reply(500, response)
     await rejects(
-      postWebhook(),
+      postWebhook(slackWebhook),
       new SemanticReleaseError(response, 'INVALID SLACK COMMAND')
     )
   })
@@ -68,6 +71,10 @@ describe('test postMessage with webhook', () => {
 })
 
 describe('test postMessage with token/channel', () => {
+  beforeEach(() => {
+    nock.cleanAll()
+  })
+
   it('should pass if response is 200 "ok"', async () => {
     nock(slackPostMessageDomain)
       .post(slackPostMessagePath)
